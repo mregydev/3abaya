260eacb3b7fce58005acee548c1f719b7e9441e2
Alaaeldeen Hamed <mregydev@gmail.com>
2025-02-09 02:07:06 +0100

    fix linting issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,3 @@
-import styles from "./page.module.css";
-
 export default async function Home() {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/items.json`
-  );
-  const text = await response.text();
-
-  //  const data=await response.json();
-  console.log(text);
   return <h1>Real Madrid</h1>;
 }
